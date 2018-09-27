67fa69d75f48d0dccd88d5e5b9b34930d1580890
庞若男 <pangruonan@czxy.com>
2018-09-27 21:46:33 +0800

    三级联动,添加属性,添加图片

--- libs/Db.php
+++ libs/Db.php
@@ -1,47 +1,49 @@
 <?php
+/*
+执行最底的SQL语句
+：预处理方式： prepare
+：非预处理方式：exec
+*/
+namespace libs;
 
-    namespace libs;
+class Db
+{
+    private static $_obj = null;
+    private function __clone(){}
 
-    class Db
-    {
-
-        private $_pdo;
-
-        private static $_obj = null;
-
-        private function __clone(){}
-
-        private function __construct()
-        {
-
-            $this->_pdo = new \PDO('mysql:host=localhost;dbname=jxshop','root','123456');
-
-            $this->_pdo->exec("set names utf8");
-
-        }
-
-        public static function make()
-        {
-            if(self::$_obj === null)
-            {
-                self::$_obj = new self;
-
-            }
-            return self::$_obj;
-        }
-
-        // 预处理
-        public function prepare($sql)
-        {
-            return $this->_pdo->prepare($sql);
-        }
-
-        // 非预处理执行sql
-        public function exec($sql)
-        {
-            return $this->_pdo->exec($sql);
-        }
-
+    private $_pdo;
+    private function __construct(){
+        // 连接数据库
+        $this->_pdo = new \PDO('mysql:host=localhost;dbname=jxshop', 'root', '123456');
+        // 设置编码
+        $this->_pdo->exec('SET NAMES utf8');
     }
 
-?>
+    // 返回唯一的对象
+    public static function make()
+    {
+        if(self::$_obj === null)
+        {
+            self::$_obj = new self;
+        }
+        return self::$_obj;
+    }
+
+    // 预处理
+    public function prepare($sql)
+    {
+        return $this->_pdo->prepare($sql);        
+    }
+
+    // 非预处理执行SQL
+    public function exec($sql)
+    {
+        return $this->_pdo->exec($sql);
+    }
+
+    // 获取最新添加的记录的ID
+    public function lastInsertId()
+    {
+        return $this->_pdo->lastInsertId();
+    }
+}
---
+++ models/Category.php
@@ -0,0 +1,19 @@
+<?php
+namespace models;
+
+class Category extends Model
+{
+    // 设置这个模型对应的表
+    protected $table = 'category';
+    // 设置允许接收的字段
+    protected $fillable = ['cat_name','parent_id','path'];
+
+    // 取出顶级分类
+    public function getCat($parent_id = 0)
+    {
+        return $this->findAll([
+            'where' => "parent_id=$parent_id"
+        ]);
+    }
+
+}
